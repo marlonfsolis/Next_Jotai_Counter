4f53257f6efb97773831276fc399b5f3d46be1ad
Marlon Fernandez <marlonfsolis@outlook.com>
2022-07-27 06:25:48 -0400

    Adding bootstrap. Adding more buttons. Extracting the global var to an external store file.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Provider } from 'jotai'
+import { useEffect } from "react";
+
+import type { AppProps } from "next/app";
+import { Provider } from "jotai";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
-  )
+    return (
+        <Provider>
+            <Component {...pageProps} />
+        </Provider>
+    );
 }
---
+++ store.ts
@@ -0,0 +1,4 @@
+import { atom } from "jotai";
+
+export const countAtom = atom(0);
+export const doubleAtom = atom((get) => get(countAtom) * 2);
